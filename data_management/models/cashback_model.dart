59c52438186f81110aa83a5c2bb31c65b6dd2043
Zhenya-Mykhalskyi <zmykhalskyi@gmail.com>
2023-11-03 17:03:02 +0200

    feat: add CashbackModel

---
+++ data_management/models/cashback_model.dart
@@ -0,0 +1,27 @@
+class CashbackModel {
+  final List<int> discountPercentage;
+  final List<int> spentMoneyLevel;
+
+  CashbackModel({
+    required this.discountPercentage,
+    required this.spentMoneyLevel,
+  });
+
+  CashbackModel.initial()
+      : discountPercentage = [0, 0, 0, 0],
+        spentMoneyLevel = [0, 0, 0, 0];
+
+  factory CashbackModel.fromMap(Map<String, dynamic> map) {
+    return CashbackModel(
+      discountPercentage: List<int>.from(map['discountPercentage']),
+      spentMoneyLevel: List<int>.from(map['spentMoneyLevel']),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'discountPercentage': discountPercentage,
+      'spentMoneyLevel': spentMoneyLevel,
+    };
+  }
+}

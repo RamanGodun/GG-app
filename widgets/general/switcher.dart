cb734d7cc35c42ef208c091e3b7c716a9b93b840
Roman Godun <romangodun@MacBook-Air-Roman.local>
2023-11-09 13:50:38 +0200

    move switcher to widgets

--- widgets/general/switcher.dart
+++ widgets/general/switcher.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'theme_provider.dart';
+import '../../data_management/theme_provider.dart';
 
 class ThemeSwitcher extends StatelessWidget {
   const ThemeSwitcher({super.key});
@@ -37,25 +37,3 @@ class ThemeSwitcher extends StatelessWidget {
     );
   }
 }
-
-// Widget build(BuildContext context) {
-//   final themeProvider = Provider.of<ThemeProvider>(context);
-//   return Switch(
-//     value: themeProvider.isDarkMode,
-//     onChanged: (value) {
-//       final provider = Provider.of<ThemeProvider>(context, listen: false);
-//       provider.toggleTheme(value);
-//       Navigator.of(context).pushAndRemoveUntil(
-//         MaterialPageRoute(
-//           builder: (context) =>
-//               const UserMainScreen(selectedBottomNavBarIndex: 0),
-//         ),
-//         (route) => false,
-//       );
-//     },
-//     activeColor: AppColors.blackColor,
-//     inactiveTrackColor: AppColors.primaryColor.withOpacity(0.3),
-//     activeThumbImage: const AssetImage('assets/img/moon.png'),
-//     inactiveThumbImage: const AssetImage('assets/img/sun.png'),
-//   );
-// }

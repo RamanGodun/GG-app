e118349425c61ac7ff8b425376c1a9d2bce52696
Roman Godun <romangodun@MacBook-Air-Roman.local>
2023-11-09 13:46:38 +0200

    delete extra comments

--- main.dart
+++ main.dart
@@ -1,11 +1,7 @@
-// general import
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-
-// screens
-// import '/splash_screen.dart';
 import 'data_management/theme_provider.dart';
 import 'screens/general/app_tabs.dart';
 
@@ -49,31 +45,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-
-//   SystemChrome.setPreferredOrientations([
-//     DeviceOrientation.portraitUp,
-//     DeviceOrientation.portraitDown,
-//   ]);
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatefulWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: AppTabs(bottomTab: 0),
-//     );
-//   }
-// }

4d1e3074b7875a0af6825d77adb077f9e6c87095
Zhenya-Mykhalskyi <zmykhalskyi@gmail.com>
2023-11-13 22:53:07 +0200

    refactor: ThemeProvider, add snackBarStyle

--- data_management/theme_provider.dart
+++ data_management/theme_provider.dart
@@ -39,65 +39,59 @@ class ThemeProvider extends ChangeNotifier {
 
 ThemeData _createDarkTheme() {
   return ThemeData.dark().copyWith(
-    primaryColor: ColorsForApp.primary,
-    canvasColor: ColorsForApp.background,
+    primaryColor: ColorsForApp.white,
+    colorScheme: const ColorScheme.dark(),
     scaffoldBackgroundColor: ColorsForApp.background,
-    hoverColor: AdditionalColorsForApp.hover,
-    focusColor: ColorsForApp.white,
-    shadowColor: AdditionalColorsForApp.silver,
     indicatorColor: AdditionalColorsForApp.amber1,
-    primaryColorLight: AdditionalColorsForApp.amber1.withOpacity(0.5),
-    colorScheme: ColorScheme.fromSwatch()
-        .copyWith(secondary: AdditionalColorsForApp.grey),
     bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-      selectedItemColor: ColorsForApp.white,
-      unselectedItemColor: AdditionalColorsForApp.hover,
-      backgroundColor: AdditionalColorsForApp.silver,
-    ),
+        selectedItemColor: ColorsForApp.white,
+        unselectedItemColor: AdditionalColorsForApp.hover,
+        backgroundColor: ColorsForApp.background),
     appBarTheme: const AppBarTheme(
-      elevation: 0,
-      backgroundColor: ColorsForApp.background,
-    ),
+        elevation: 0, backgroundColor: ColorsForApp.background),
+    snackBarTheme: const SnackBarThemeData(
+        backgroundColor: Color.fromARGB(255, 51, 51, 51),
+        contentTextStyle:
+            TextStyle(color: ColorsForApp.white, fontFamily: 'MA')),
     textTheme: const TextTheme(
-      bodyMedium: TextStyle(fontFamily: 'MA'),
+      bodyMedium: TextStyle(fontFamily: 'MA', color: ColorsForApp.white),
     ),
   );
 }
 
 ThemeData _createLightTheme() {
   return ThemeData.light().copyWith(
-    primaryColor: ColorsForApp.primary,
-    colorScheme: ColorScheme.fromSwatch()
-        .copyWith(secondary: AdditionalColorsForApp.silver),
-    focusColor: ColorsForApp.black.withOpacity(0.5),
-    hoverColor: AdditionalColorsForApp.lighthover,
-    indicatorColor: AdditionalColorsForApp.amber1,
-    canvasColor: ColorsForApp.white,
+    primaryColor: ColorsForApp.black,
+    colorScheme: const ColorScheme.light(),
     scaffoldBackgroundColor: ColorsForApp.white,
+    indicatorColor: AdditionalColorsForApp.amber1,
     bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-      selectedItemColor: ColorsForApp.black,
-      unselectedItemColor: AdditionalColorsForApp.bottomNavBarUnselectedLight,
-      backgroundColor: ColorsForApp.white,
-    ),
+        selectedItemColor: ColorsForApp.black,
+        unselectedItemColor: AdditionalColorsForApp.bottomNavBarUnselectedLight,
+        backgroundColor: Color.fromARGB(255, 255, 255, 255)),
     appBarTheme: const AppBarTheme(
-      color: ColorsForApp.white,
-      iconTheme: IconThemeData(color: ColorsForApp.black),
-      elevation: 0,
+        color: ColorsForApp.white,
+        iconTheme: IconThemeData(color: ColorsForApp.black),
+        elevation: 0),
+    snackBarTheme: const SnackBarThemeData(
+        backgroundColor: AdditionalColorsForApp.lightSnackBar,
+        contentTextStyle:
+            TextStyle(color: ColorsForApp.black, fontFamily: 'MA')),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(fontFamily: 'MA', color: ColorsForApp.black),
     ),
-    textTheme: const TextTheme(bodyMedium: TextStyle(fontFamily: 'MA')),
   );
 }
 
 class ColorsForApp {
-  static const Color white = Color(0xFFD9D9D9);
+  static const Color white = Colors.white;
   static const Color primary = Color(0xFFFFB100);
   static const Color background = Color(0xFF1B1B1B);
   static const Color black = Colors.black;
 }
 
 class AdditionalColorsForApp {
-  static const Color white = Color(0xFFFFFFFF);
-
+  static const Color lightSnackBar = Color.fromARGB(255, 225, 225, 225);
   static const Color silver = Color(0xFF999A9B);
   static const Color lightGrey = Color(0xFF777777);
   static const Color shadow = Color(0xFF999A9B);
